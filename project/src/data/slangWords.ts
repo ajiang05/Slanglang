783d8968642f58a3de34f95cdd19ffd8ff6d27a2
Aidan Jiang <markedaidan1010@gmail.com>
2025-02-15 15:55:23 -0500

    Added more words

--- project/src/data/slangWords.ts
+++ project/src/data/slangWords.ts
@@ -47,15 +47,45 @@ export const slangWords = [
     definition: "toilet",
     example: "Skibidi toilet skibidi ",
     difficulty: "hard",
-    points: 20,
+    points: 10,
   },
 
   {
     id: 7,
-    word: "rent free",
-    definition: "When something or someone is constantly on your mind",
-    example: "That song is living rent free in my head.",
+    word: "pmo",
+    definition: "Piss me off ",
+    example: "this is really pmo",
+    difficulty: "hard",
+    points: 10,
+  },
+
+  {
+    id: 8,
+    word: "aura",
+    definition: "How cool you look",
+    example: "My friend has a mad aura",
     difficulty: "medium",
-    points: 100000,
+    points: 10,
+  },
+
+  {
+    id: 9,
+    word: "gyat",
+    definition: "A gluteus maximus",
+    example: "He has a great gyat",
+    difficulty: "hard",
+    points: 10,
+  },
+
+  {
+    id: 10,
+    word: "gassed",
+    definition: "To be really happy or excited",
+    example: "I was so gassed when I got the job",
+  },
+  {
+    id: 11,
+    word: "peng",
+    definition: "Something that is really good or attractive",
   },
 ] as const;
